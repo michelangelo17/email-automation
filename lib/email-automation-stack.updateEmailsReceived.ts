ffc8f3df5eab0466b7d4849d02163b90e16fae0c
Michelangelo Markus <michelangelo.markus@superluminar.io>
2025-01-06 23:22:51 +0100

    fix initialization for oauth

--- lib/email-automation-stack.updateEmailsReceived.ts
+++ lib/email-automation-stack.updateEmailsReceived.ts
@@ -4,18 +4,18 @@ import { google } from 'googleapis'
 const dynamo = new DynamoDB({})
 const tableName = process.env.EMAILS_TABLE_NAME!
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GMAIL_CLIENT_ID!,
-  process.env.GMAIL_CLIENT_SECRET!
-)
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.GMAIL_REFRESH_TOKEN!,
-})
-
-const gmail = google.gmail({ version: 'v1', auth: oauth2Client })
-
 export const handler = async (event: any) => {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GMAIL_CLIENT_ID!,
+    process.env.GMAIL_CLIENT_SECRET!
+  )
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN!,
+  })
+
+  const gmail = google.gmail({ version: 'v1', auth: oauth2Client })
+
   const { missingEmails } = event
 
   if (!missingEmails || missingEmails.length === 0) {
